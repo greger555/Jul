783665e2dc6442769517a77a01570c91f196ea83
greger555 <121067501+greger555@users.noreply.github.com>
2023-02-05 23:16:41 +0100

    Add files via upload

---
+++ index.JS
@@ -0,0 +1,4 @@
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
+console.log("Screen width: " + screenWidth + "px");
+console.log("Screen height: " + screenHeight + "px");
